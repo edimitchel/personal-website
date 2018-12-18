7bad6a2dab717c6ee3e103dab157b456a72086b9
edimitchel <edimitchel@gmail.com>
2018-12-18 11:59:23 +0100

    Initial commit

---
+++ .vuepress/theme/layouts/HomeLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-center">Home
+    <Cards/>
+  </div>
+</template>
+
+<script>
+import Cards from "../components/Cards.vue";
+
+export default {
+  components: { Cards },
+  name: "HomeLayout"
+};
+</script>
---
+++ README.md
@@ -0,0 +1,6 @@
+---
+home: true
+heroText: Michel EDIGHOFFER
+tagline: Developer Advocate | Speaker | Author | Builder of Interesting Things
+layout: HomeLayout
+---
