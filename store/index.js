14059d529e7e9a55ee784640c8467b1d0ee7e1e8
Michel EDIGHOFFER <edimitchel@gmail.com>
2020-01-03 13:06:58 +0100

    Simplify Storyblok token accessment

--- store/index.js
+++ store/index.js
@@ -1,11 +1,16 @@
 export const actions = {
-  async nuxtServerInit({ dispatch }, { params, env: { isDev } }) {
+  async nuxtServerInit(
+    { dispatch },
+    { params, env: { isDev }, app: { $storyapi } }
+  ) {
     const { lang } = params
-    const token = process.env.STORYBLOK_API_KEY
-    await dispatch('informations/load', {
-      token,
-      isDev,
-      language: lang
-    })
+    if ($storyapi) {
+      const token = $storyapi.accessToken || process.env.STORYBLOK_API_KEY
+      await dispatch('informations/load', {
+        token,
+        isDev,
+        language: lang
+      })
+    }
   }
 }
