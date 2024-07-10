71b613698c44c0c55c65fecdd7a5e571f2650fc5
Michel EDIGHOFFER <edimitchel@gmail.com>
2024-07-11 00:00:51 +0200

    feat: push app config + few change on schema

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -7,13 +7,49 @@ export default defineNuxtSchema({
         description: 'Information of the website',
         icon: 'i-mdi-earth',
         fields: {
-          title: field({
-            type: 'string',
-            title: 'Title of the website',
-            description: '',
-            icon: 'i-mdi-text-short',
-            default: ''
-          }),
+            title: field({
+              type: 'string',
+              title: 'Title of the website',
+              description: '',
+              icon: 'i-mdi-text-short',
+              default: ''
+            }),
+            description: field({
+              type: 'string',
+              title: 'Description of the website',
+              description: '',
+              icon: 'i-mdi-text',
+              default: ''
+            }),
+            github: field({
+              type: 'string',
+              title: 'Github account',
+              description: '',
+              icon: 'i-mdi-text',
+              default: ''
+            }),
+            twitter: field({
+              type: 'string',
+              title: 'Twitter / X account',
+              description: '',
+              icon: 'i-mdi-text',
+              default: ''
+            }),
+        }
+      }),
+
+      ui: group({
+        title: 'UI',
+        description: 'Visual stuff of the website',
+        icon: 'i-mdi-drawing-box',
+        fields: {
+            icons: field({
+              type: 'array',
+              title: 'Icons',
+              description: 'Icons used between full name',
+              icon: 'i-mdi-image-filter-vintage',
+              default: ''
+            }),
         }
       })
     }
