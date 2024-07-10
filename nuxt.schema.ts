aaa7180ee8e0f556988bd439cb77394d3feef075
Michel EDIGHOFFER <edimitchel@gmail.com>
2024-07-10 23:45:57 +0200

    feat: add nuxt schema

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -1,2 +1,20 @@
+import { field, group } from "@nuxthq/studio/theme";
+
 export default defineNuxtSchema({
-})
+    appConfig: {
+      information: group({
+        title: 'Website information',
+        description: 'Information of the website',
+        icon: 'i-mdi-earth',
+        fields: {
+          title: field({
+            type: 'string',
+            title: 'Title of the website',
+            description: '',
+            icon: 'i-mdi-text-short',
+            default: ''
+          }),
+        }
+      })
+    }
+  })
