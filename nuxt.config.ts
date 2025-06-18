6d0e40ab34f75f75d553520da412e6ff3d4fe919
Michel EDIGHOFFER <edimitchel@gmail.com>
2025-06-18 16:36:00 +0200

    fix: nuxt studio setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,7 +44,9 @@ export default defineNuxtConfig({
   css: ['~/assets/style.css'],
 
   content: {
-    documentDriven: true
+    preview: {
+      api: 'https://api.nuxt.studio'
+    }
   },
 
   nitro: {
